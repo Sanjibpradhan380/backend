68f42da97dc8e4bc64896fe16275dec75031e395
Sanjibpradhan380 <sanjibpradhan380@gmail.com>
2026-03-26 19:31:34 +0530

    fourth commit

--- src/app.js
+++ src/app.js
@@ -59,7 +59,7 @@ app.patch("/api/notes/:id", async (req,res)=>{
 })
 
 app.use('*name',(req,res)=>{
-       res.sendFile(path.join(__dirname,"/public/index.html"))
+       res.sendFile(path.join(__dirname,"..","/public/index.html"))
 })
 
 module.exports = app
